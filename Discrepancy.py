8ba3cf4aeabd5b93d1e43817ae09708ba9654f62
TesLineX <teslinex@gmail.com>
2024-04-13 12:08:12 +0200

    Discrepancy

--- Discrepancy.py
+++ Discrepancy.py
@@ -5,10 +5,11 @@ endDate = '2024-04-30'
 dsp_id = '12482'
 dsp_part_id = '45649'
 token = ''
+dsp_token = ''
 
 
 def link_1(start_date, end_date):
-  text = f'https://daopush-api.info/api/statistic/pops-web?api-key=ms4lZG0x86s2RymbEs415YqFhbUTRpve&groupBy=days&sourceId={dsp_part_id}&startDate={start_date}&endDate={end_date}&timeZone=UTC'
+  text = f'https://daopush-api.info/api/statistic/pops-web?api-key={dsp_token}&groupBy=days&sourceId={dsp_part_id}&startDate={start_date}&endDate={end_date}&timeZone=UTC'
   return text
 
 
@@ -32,20 +33,20 @@ data_2 = response.json()
 df_rp = pd.DataFrame(data_2['data'])
 df_join = df_dsp.merge(df_rp, how='inner', left_on='period', right_on='date', suffixes=('_x', '_y'))
 df_join.drop(['uniqs', 'hits', 'requests_x', 'requests_y', 'responses', 'win_responses', 'currency', 'period'], axis = 1, inplace = True)
-df_join.rename(columns = {'date': 'Date'}, inplace = True)
-df_join.rename(columns = {'clicks_x': 'DaoPush Clicks'}, inplace = True)
-df_join.rename(columns = {'pay': 'DaoPush Revenue'}, inplace = True)
-df_join.rename(columns = {'clicks_y': 'RealPush Clicks'}, inplace = True)
-df_join.rename(columns = {'amount': 'RealPush Revenue'}, inplace = True)
-df_join.rename(columns = {'publisher_amount': 'Publisher Revenue'}, inplace = True)
-df_join.fillna(0, inplace = True)
+df_join.rename(columns={'date': 'Date'}, inplace=True)
+df_join.rename(columns={'clicks_x': 'DaoPush Clicks'}, inplace=True)
+df_join.rename(columns={'pay': 'DaoPush Revenue'}, inplace=True)
+df_join.rename(columns={'clicks_y': 'RealPush Clicks'}, inplace=True)
+df_join.rename(columns={'amount': 'RealPush Revenue'}, inplace=True)
+df_join.rename(columns={'publisher_amount': 'Publisher Revenue'}, inplace=True)
+df_join.fillna(0, inplace=True)
 df_join['CLK DSC %'] = ((1 - (1 / df_join['RealPush Clicks'] * df_join['DaoPush Clicks'])) * 100)
 df_join['REV DSC %'] = ((1 - (1 / df_join['RealPush Revenue'] * df_join['DaoPush Revenue'])) * 100)
 df_join['DaoPush Revenue'] = df_join['DaoPush Revenue'].round(2)
 df_join['RealPush Revenue'] = df_join['RealPush Revenue'].round(2)
 df_join['Publisher Revenue'] = df_join['Publisher Revenue'].round(2)
 df_join['PROFIT'] = (df_join['DaoPush Revenue'] - df_join['Publisher Revenue']).round(2)
-df_join.fillna(0, inplace = True)
+df_join.fillna(0, inplace=True)
 df_join.loc[len(df_join.index)] = [df_join['DaoPush Revenue'].sum(),
                                    df_join['DaoPush Clicks'].sum(),
                                    'Total',
